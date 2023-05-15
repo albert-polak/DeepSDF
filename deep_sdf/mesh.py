1221d9b1787faa1db53b04d9995f18d9cdc6c853
Albert Polak <lugii1999@gmail.com>
2023-05-15 15:05:25 +0200

    fix

--- deep_sdf/mesh.py
+++ deep_sdf/mesh.py
@@ -59,8 +59,8 @@ def create_mesh(
         head += max_batch
 
     sdf_values = samples[:, 3]
-    sdf_values = sdf_values.reshape((N // 2), (N // 2), (N // 2))
-    # sdf_values = sdf_values.reshape(samples.shape[0])
+    # sdf_values = sdf_values.reshape(N, N, N)
+    sdf_values = sdf_values.reshape(samples.shape[0], samples.shape[0], samples.shape[0])
 
     end = time.time()
     print("sampling takes: %f" % (end - start))
