d55359fdf356dea4dd98e8e892611f2501d607bd
Albert Polak <lugii1999@gmail.com>
2023-05-19 12:22:37 +0200

    test

--- deep_sdf/mesh.py
+++ deep_sdf/mesh.py
@@ -11,7 +11,7 @@ import torch
 import deep_sdf.utils
 
 
-def create_mesh(
+def create_mesh_org(
     decoder, latent_vec, filename, N=256, max_batch=32 ** 3, offset=None, scale=None
 ):
     start = time.time()
@@ -32,8 +32,6 @@ def create_mesh(
     samples[:, 1] = (overall_index.long() / N) % N
     samples[:, 0] = ((overall_index.long() / N) / N) % N
 
-    top_region_mask = samples[:, 0] >= (N // 2)  # Modify the condition as per your requirement
-    samples = samples[top_region_mask]
 
     print(len(samples[:, 2]))
     print(len(samples[:, 1]))
@@ -63,8 +61,70 @@ def create_mesh(
         head += max_batch
 
     sdf_values = samples[:, 3]
-    # sdf_values = sdf_values.reshape(N, N, N)
-    sdf_values = sdf_values.reshape(samples.shape[0], samples.shape[0], samples.shape[0])
+    sdf_values = sdf_values.reshape(N, N, N)
+
+    end = time.time()
+    print("sampling takes: %f" % (end - start))
+
+    convert_sdf_samples_to_ply(
+        sdf_values.data.cpu(),
+        voxel_origin,
+        voxel_size,
+        ply_filename + ".ply",
+        offset,
+        scale,
+    )
+
+def create_mesh(
+    decoder, latent_vec, filename, N=256, max_batch=32 ** 3, offset=None, scale=None
+):
+    start = time.time()
+    ply_filename = filename
+
+    decoder.eval()
+
+    # NOTE: the voxel_origin is actually the (bottom, left, down) corner, not the middle
+    voxel_origin = [-1, -1, -1]
+    voxel_size = 2.0 / (N - 1)
+
+    overall_index = torch.arange(0, N ** 3, 1, out=torch.LongTensor())
+    samples = torch.zeros(N ** 3, 4)
+
+    # transform first 3 columns
+    # to be the x, y, z index
+    samples[:, 2] = overall_index % N
+    samples[:, 1] = (overall_index.long() / N) % N
+    samples[:, 0] = ((overall_index.long() / N) / N) % N
+
+    # Filter out samples that are not in the top region
+    top_region_mask = samples[:, 0] >= (N / 2)
+    samples = samples[top_region_mask]
+
+    # transform first 3 columns
+    # to be the x, y, z coordinate
+    samples[:, 0] = (samples[:, 0] * voxel_size) + voxel_origin[2]
+    samples[:, 1] = (samples[:, 1] * voxel_size) + voxel_origin[1]
+    samples[:, 2] = (samples[:, 2] * voxel_size) + voxel_origin[0]
+
+    num_samples = samples.shape[0]
+
+    samples.requires_grad = False
+
+    head = 0
+
+    while head < num_samples:
+        sample_subset = samples[head : min(head + max_batch, num_samples), 0:3].cuda()
+
+        samples[head : min(head + max_batch, num_samples), 3] = (
+            deep_sdf.utils.decode_sdf(decoder, latent_vec, sample_subset)
+            .squeeze(1)
+            .detach()
+            .cpu()
+        )
+        head += max_batch
+
+    sdf_values = samples[:, 3]
+    sdf_values = sdf_values.reshape(N, N, N)
 
     end = time.time()
     print("sampling takes: %f" % (end - start))
